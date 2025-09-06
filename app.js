de701da137fa3064f79dee2987e9997967fa23cf
MohdFaisal21 <faisalmohd1357@gmail.com>
2025-09-06 16:35:25 +0530

    updated stop watch

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let timeDisplay = document.querySelector(".timeDisplay");
 
 let start = document.querySelector(".start");
+let pause = document.querySelector(".pause");
+let reset = document.querySelector(".reset");
 
 let setId = null;
 let isStart = false;
 let resume = false;
+
+// Start Button Code
+
 start.addEventListener("click", function(){
     if(!isStart){
+        isStart = true;
+        resume = false
+        pause.classList.toggle("display");
+        setId = setInterval(changeTime, 10);
+    }else{
+        res();
         isStart = true;
         resume = false
         setId = setInterval(changeTime, 10);
@@ -28,10 +39,13 @@ function changeTime(){
     let str = `${min<10?'0'+min: min} : ${sec<10?'0'+sec: sec} : ${msec<10?'0'+msec: msec}`;
     timeDisplay.innerText = str;
 };
-let pause = document.querySelector(".pause");
+
+// Pause Button code
 
 pause.addEventListener("click", function(){
     resume = !resume;
+    start.classList.toggle("hidden");
+    reset.classList.toggle("display");
     if(isStart){
         if(resume){
             clearInterval(setId);
@@ -39,18 +53,21 @@ pause.addEventListener("click", function(){
             setId = setInterval(changeTime, 10);
         }
     }
-        
 });
 
-let reset = document.querySelector(".reset");
-
+// Reset Button Code
 reset.addEventListener("click", function(){
+    res();
+    start.classList.toggle("hidden");
+    pause.classList.toggle("display");
+    reset.classList.toggle("display");
+});
+
+function res(){
     timeDisplay.innerText = `00 : 00 : 00`;
     msec = 0;
     sec = 0; 
     min = 0;
     clearInterval(setId);
     isStart = false;
-});
-
-if(!isStart)  clearInterval("setId");
+}
